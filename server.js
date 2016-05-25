3e57902dcfdeeade3debdcffec439c485887acce
ravsh-bingo <4ravishanker@gmail.com>
2016-05-25 20:35:55 +0530

    middle init

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var express=require("express");
 var app=express();
 var port=4000; 
- var middleware={
-   requireAuthentictaion: function(req,res,next){
-
-console.log("private route hit!");
-next();     
-   },
-   logger:function(req,res,next){
-     console.log("Request:"+' '+new Date().toString() +' '+req.method+' '+req.originalUrl);
-     next(); 
-   }
- };
+ var middleware=require('./middleware.js');
  app.use(middleware.logger);
  app.use(middleware.requireAuthentictaion);
   app.get('/about',middleware.requireAuthentictaion,function (req,res) {
